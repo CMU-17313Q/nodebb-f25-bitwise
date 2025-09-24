abc6b1c35eb66763c7e7e85fd7405e008f9ae8e4
Hazem Elsayed <helsayed@avey.ai>
2025-09-24 11:34:45 +0300

    add tools to check for privilege and mark posts official

--- src/posts/tools.js
+++ src/posts/tools.js
@@ -13,6 +13,22 @@ module.exports = function (Posts) {
 		return await togglePostDelete(uid, pid, false);
 	};
 
+	Posts.tools.markOfficial = async function (uid, pid) {
+		return await togglePostOfficial(uid, pid, true);
+	};
+
+	Posts.tools.unmarkOfficial = async function (uid, pid) {
+		return await togglePostOfficial(uid, pid, false);
+	};
+
+	Posts.tools.toggleOfficial = async function (uid, pid) {
+		const postData = await Posts.getPostData(pid);
+		if (!postData) {
+			throw new Error('[[error:no-post]]');
+		}
+		return await togglePostOfficial(uid, pid, !postData.official);
+	};
+
 	async function togglePostDelete(uid, pid, isDelete) {
 		const [postData, canDelete] = await Promise.all([
 			Posts.getPostData(pid),
@@ -40,5 +56,41 @@ module.exports = function (Posts) {
 			post = await Posts.parsePost(post);
 		}
 		return post;
+	}
+
+	async function togglePostOfficial(uid, pid, isOfficial) {
+		const [postData, canMarkOfficial] = await Promise.all([
+			Posts.getPostData(pid),
+			privileges.posts.canMarkOfficial(pid, uid),
+		]);
+
+		if (!postData) {
+			throw new Error('[[error:no-post]]');
+		}
+
+		if (postData.official && isOfficial) {
+			throw new Error('[[error:post-already-official]]');
+		} else if (!postData.official && !isOfficial) {
+			throw new Error('[[error:post-not-official]]');
+		}
+
+		if (!canMarkOfficial) {
+			throw new Error('[[error:no-privileges]]');
+		}
+
+		await Posts.setPostField(pid, 'official', isOfficial ? 1 : 0);
+
+		// Clear cached post data to ensure updated official field is loaded
+		Posts.clearCachedPost(pid);
+
+		const plugins = require('../plugins');
+		await plugins.hooks.fire('action:post.officialToggled', {
+			pid: pid,
+			uid: uid,
+			official: isOfficial,
+		});
+
+		const updatedPost = await Posts.getPostData(pid);
+		return await Posts.parsePost(updatedPost);
 	}
 };
